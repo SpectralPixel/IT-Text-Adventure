c9be9216a8f98266150c2f265abf24d191a78067
SpectralPixel <encrypteddvjjrxv@gmail.com>
2023-12-17 14:26:57 +0100

    all elements displayed on screen

--- IT Text Adventure Emil Engelbert 2023.py
+++ IT Text Adventure Emil Engelbert 2023.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk, Image
 import json
 
 
@@ -20,13 +21,22 @@ window = Tk()
 window.title("Text Adventure")
 window.geometry("777x444")
 
-winImage = PhotoImage() # space to display images
-winTitle = Label() # title of the current scene
-winText = Label() # main text with the story from the current scene
-winField = Label() # text field to input commands
-winReset = Label() # reset button
+winTitle = Label(window, text = "hello world\nnewline!!!") # title of the current scene
+winCanvas = Canvas(window, width = 80, height = 80, bg = 'white') # space to display images
+winImage = ImageTk.PhotoImage(file = "coconut.jpg")
+winText = Label(window, text = "hello world\nnewline!!!\nthis is the text field") # main text with the story from the current scene
+winEntry = Entry() # text field to input commands
+winOptions = Label(text = ">Option1\n>Option2") # list of all possible commands
+winConfirm = Button(text = "Enter") # confirm button
+winReset = Button(text = "Reset") # reset button
 
-
+winTitle.grid(row = 0, column = 0, columnspan = 6)
+winCanvas.grid(row = 1, column = 0, columnspan = 6)
+winCanvas.create_image(0, 0, image = winImage)
+winText.grid(row = 2, column = 0, rowspan = 3, columnspan = 4)
+winOptions.grid(row = 2, column = 5, columnspan = 2)
+winConfirm.grid(row = 3, column = 5)
+winReset.grid(row = 3, column = 6)
 
 #endregion ----------
 
